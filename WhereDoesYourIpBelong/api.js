5e8891298f4647ad4f6c2da9187507e7ff6db723
nylus1 <ivb.ignacio@gmail.com>
2023-04-07 02:31:26 +0200

    Added a button that catches the ip info, and generates a txt file that you can download.

--- WhereDoesYourIpBelong/api.js
+++ WhereDoesYourIpBelong/api.js
@@ -3,7 +3,12 @@ const ipInput = document.getElementById('ipInputfield');
 const ipBtn = document.getElementById('ipBtn');
 const ipInfoDiv = document.getElementById('ipInfo');
 
-// Function to display IP information in HTML
+// Fetch IP information for the user's own IP when the page loads
+fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(data => displayIpInfo(data));
+
+// Function to display IP information in HTML and create download button
 function displayIpInfo(data) {
     // Create an array of objects for the IP data
     const ipData = [
@@ -17,11 +22,10 @@ function displayIpInfo(data) {
         { label: 'Longitude', value: data.longitude },
         { label: 'Timezone', value: data.timezone },
         { label: 'Organization', value: data.org },
-        { label: 'Country Calling Code', value: data.country_calling_code	 },
-        { label: 'Currency', value: data.currency_name	 },
-        { label: 'Time Zone', value: data.timezone	 },
-        { label: 'Country Specific TLD (top-level domain)', value: data.country_tld	 }
-
+        { label: 'Country Calling Code', value: data.country_calling_code },
+        { label: 'Currency', value: data.currency_name },
+        { label: 'Time Zone', value: data.timezone },
+        { label: 'Country Specific TLD (top-level domain)', value: data.country_tld }
     ];
 
     // Create a list element for the IP data
@@ -34,15 +38,28 @@ function displayIpInfo(data) {
         ipList.appendChild(listItem);
     });
 
-    // Clear any existing IP information from the div and add the new list
+    // Get the download button for the IP download
+    const downloadBtn = document.getElementById('downloadButton');
+    downloadBtn.addEventListener('click', () => {
+        // Create a Blob object with the IP data in text format
+        const dataBlob = new Blob([ipData.map(item => `${item.label}: ${item.value}`).join('\n')], { type: 'text/plain' });
+
+        // Create a download link for the Blob object
+        const downloadLink = document.createElement('a');
+        downloadLink.href = URL.createObjectURL(dataBlob);
+        downloadLink.download = 'ip_info.txt';
+
+        // Add the download link to the page and click it programmatically
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+
+    // Clear any existing IP information from the div and add the new list and download button
     ipInfoDiv.innerHTML = '';
     ipInfoDiv.appendChild(ipList);
+    ipInfoDiv.appendChild(downloadBtn);
 }
-
-// Fetch IP information for the user's own IP when the page loads
-fetch('https://ipapi.co/json/')
-    .then(response => response.json())
-    .then(data => displayIpInfo(data));
 
 // Add event listener to search button
 ipBtn.addEventListener('click', () => {
@@ -53,4 +70,5 @@ ipBtn.addEventListener('click', () => {
         .then(response => response.json())
         .then(data => displayIpInfo(data))
         .catch(error => console.log(error));
+
 });
